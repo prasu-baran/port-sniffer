7d47e9dd23feadc1dda991d6d222c1cfb250d1c3
Prasun Baranwal <prasubaran@gmail.com>
2025-08-12 23:34:18 +0530

    port address

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use std::{env, net::IpAddr, str::FromStr};
+use std::{
+    env,
+    net::{IpAddr, SocketAddr, TcpStream},
+    str::FromStr,
+    thread,
+    time::Duration,
+};
 
 struct Arguments {
     flag: String,
@@ -25,13 +31,18 @@ impl Arguments {
             let flag = args[1].clone();
             if (flag.contains("-h") || flag.contains("-help")) && args.len() == 2 {
                 println!(
-                    "Usage: -j to select how many threads you want\n\
-                     -h or -help to show this help message"
+                    "Usage:\n\
+                     <ip>                 Scan with default 4 threads\n\
+                     -j <threads> <ip>    Scan with custom number of threads\n\
+                     -h / -help           Show this help message"
                 );
                 return Err("help");
             } else if flag.contains("-h") || flag.contains("-help") {
                 return Err("too many arguments");
             } else if flag.contains("-j") {
+                if args.len() != 4 {
+                    return Err("Usage: -j <threads> <ip>");
+                }
                 let ipaddr = match IpAddr::from_str(&args[3]) {
                     Ok(s) => s,
                     Err(_) => return Err("not a valid IPADDR; must be IPv4 or IPv6"),
@@ -51,6 +62,16 @@ impl Arguments {
     }
 }
 
+// Port scanning worker
+fn scan_ports(ip: IpAddr, start_port: u16, step: u16) {
+    for port in (start_port..=65535).step_by(step as usize) {
+        let addr = SocketAddr::new(ip, port);
+        if TcpStream::connect_timeout(&addr, Duration::from_millis(200)).is_ok() {
+            println!("Port {} is OPEN", port);
+        }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
@@ -58,10 +79,30 @@ fn main() {
     match Arguments::new(&args) {
         Ok(arguments) => {
             println!("Program: {}", program);
-            println!("Flag: {}", arguments.flag);
-            println!("IP Address: {}", arguments.ipaddr);
+            println!("Scanning IP: {}", arguments.ipaddr);
             println!("Threads: {}", arguments.threads);
+            println!("Starting scan...\n");
+
+            let mut handles = vec![];
+
+            for i in 0..arguments.threads {
+                let ip = arguments.ipaddr.clone();
+                let threads = arguments.threads;
+                handles.push(thread::spawn(move || {
+                    scan_ports(ip, i + 1, threads);
+                }));
+            }
+
+            for h in handles {
+                h.join().unwrap();
+            }
+
+            println!("\nScan complete.");
         }
-        Err(e) => eprintln!("Error: {}", e),
+        Err(e) => {
+            if e != "help" {
+                eprintln!("Error: {}", e);
+            }
+        }
     }
 }
